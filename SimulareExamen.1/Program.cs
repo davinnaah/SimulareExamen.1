a93d13b23e799f74792f2c256a89c7f44df8d387
User <User@DESKTOP-PQKU9A0>
2022-02-07 06:29:50 +0200

    Add project files.

---
+++ SimulareExamen.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SimulareExamen._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int fact = Recursivitate.Fact(5);
+            Console.WriteLine($"Fact({n}) = {fact}");
+        }
+    }
+}
---
+++ SimulareExamen.1/Recursivitate.cs
@@ -0,0 +1,14 @@
+﻿namespace SimulareExamen._1
+{
+    public static class Recursivitate
+    {
+        public static int Fact(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+            return n * Fact(n - 1);
+        }
+    }
+}
